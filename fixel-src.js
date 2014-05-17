628893bef8513bd761c375f26e47385742504d2a
kevin von flotow <vonflow@gmail.com>
2014-05-17 06:09:58 -0500

    switched to spaces instead of tabs

--- fixel-src.js
+++ fixel-src.js
@@ -3,171 +3,171 @@
  *  MIT license
  */
 ;( function ( window )
-	{
-		if ( !window )
-		{
-			// window is not set - trying to run in nodejs or something? hmmm
+    {
+        if ( !window )
+        {
+            // window is not set - trying to run in nodejs or something? hmmm
             return console.log( 'fixel: fatal! window object was not found' )
-		}
+        }
 
-		// fixel = fixed element
+        // fixel = fixed element
 
-		/** @constructor */
-		function Fixel( elem, opts )
-		{
-			this.elem = elem
+        /** @constructor */
+        function Fixel( elem, opts )
+        {
+            this.elem = elem
 
-			this.opts = {
-				top: 0,
+            this.opts = {
+                top: 0,
 
-				zIndex: 1
-			}
+                zIndex: 1
+            }
 
-			for ( var key in opts )
-				this.opts[ key ] = opts[ key ]
+            for ( var key in opts )
+                this.opts[ key ] = opts[ key ]
 
-			this.visible = false
+            this.visible = false
 
-			this.fixed = null
+            this.fixed = null
 
-			this._resizeHandler = this.resizeHandler.bind( this )
+            this._resizeHandler = this.resizeHandler.bind( this )
 
-			this._scrollHandler = this.scrollHandler.bind( this )
+            this._scrollHandler = this.scrollHandler.bind( this )
 
-			this.fix()
-		}
+            this.fix()
+        }
 
-		Fixel.prototype = {
-			fix: function ()
-			{
-				if ( !this.elem || this.fixed )
-				{
-					return
-				}
+        Fixel.prototype = {
+            fix: function ()
+            {
+                if ( !this.elem || this.fixed )
+                {
+                    return
+                }
 
-				this.fixed = this.elem.cloneNode( true )
+                this.fixed = this.elem.cloneNode( true )
 
-				// remove id tag since id must be unique
-				this.fixed.removeAttribute( 'id' )
+                // remove id tag since id must be unique
+                this.fixed.removeAttribute( 'id' )
 
-				this.fixed.style.setProperty( 'position', 'fixed' )
+                this.fixed.style.setProperty( 'position', 'fixed' )
 
-				this.fixed.style.setProperty( 'z-index', this.opts.zIndex )
+                this.fixed.style.setProperty( 'z-index', this.opts.zIndex )
 
-				this.fixed.style.setProperty( 'top', this.opts.top )
+                this.fixed.style.setProperty( 'top', this.opts.top )
 
-				this.setPos()
+                this.setPos()
 
-				this.setVisible( false )
+                this.setVisible( false )
 
-				// insert before this.elem
-				this.elem.parentNode.insertBefore( this.fixed, this.elem )
+                // insert before this.elem
+                this.elem.parentNode.insertBefore( this.fixed, this.elem )
 
-				window.addEventListener( 'scroll', this._scrollHandler, false )
+                window.addEventListener( 'scroll', this._scrollHandler, false )
 
-				// fire scrollHandler once
-				this.scrollHandler()
-			},
+                // fire scrollHandler once
+                this.scrollHandler()
+            },
 
-			unfix: function ()
-			{
-				window.removeEventListener( 'scroll', this._scrollHandler )
+            unfix: function ()
+            {
+                window.removeEventListener( 'scroll', this._scrollHandler )
 
-				// hide the element, in case removing doesn't work for some reason
-				// setVisible false also removes the resize handler
-				this.setVisible( false )
+                // hide the element, in case removing doesn't work for some reason
+                // setVisible false also removes the resize handler
+                this.setVisible( false )
 
-				// attempt to remove the element
-				this.fixed.remove()
+                // attempt to remove the element
+                this.fixed.remove()
 
-				// set fixed to null so this.fix() will work properly next time it's called
-				this.fixed = null
-			},
+                // set fixed to null so this.fix() will work properly next time it's called
+                this.fixed = null
+            },
 
-			setDir: function ( dir, num )
-			{
-				num = typeof num !== 'undefined' ? num : this.elem.getBoundingClientRect()[ dir ]
+            setDir: function ( dir, num )
+            {
+                num = typeof num !== 'undefined' ? num : this.elem.getBoundingClientRect()[ dir ]
 
-				this.fixed.style.setProperty( dir, num + 'px' )
-			},
+                this.fixed.style.setProperty( dir, num + 'px' )
+            },
 
-			setLeft: function ( num )
-			{
-				this.setDir( 'left', num )
-			},
+            setLeft: function ( num )
+            {
+                this.setDir( 'left', num )
+            },
 
-			setRight: function ( num )
-			{
-				num = typeof num !== 'undefined' ? num : this.elem.getBoundingClientRect().right
+            setRight: function ( num )
+            {
+                num = typeof num !== 'undefined' ? num : this.elem.getBoundingClientRect().right
 
-				num = num - this.elem.parentNode.clientWidth
+                num = num - this.elem.parentNode.clientWidth
 
-				this.setDir( 'right', num )
-			},
+                this.setDir( 'right', num )
+            },
 
-			setPos: function ()
-			{
-				var rect = this.elem.getBoundingClientRect()
+            setPos: function ()
+            {
+                var rect = this.elem.getBoundingClientRect()
 
-				this.setLeft( rect.left )
+                this.setLeft( rect.left )
 
-				this.setRight( rect.right )
-			},
+                this.setRight( rect.right )
+            },
 
-			setVisible: function ( bool )
-			{
-				var fixedDisplay,
+            setVisible: function ( bool )
+            {
+                var fixedDisplay,
 
-					elemVis
+                    elemVis
 
-				if ( bool )
-				{
-					fixedDisplay = 'block'
+                if ( bool )
+                {
+                    fixedDisplay = 'block'
 
-					elemVis = 'hidden'
+                    elemVis = 'hidden'
 
-					this.setPos()
+                    this.setPos()
 
-					window.addEventListener( 'resize', this._resizeHandler, false )
-				}
-				else
-				{
-					fixedDisplay = 'none'
+                    window.addEventListener( 'resize', this._resizeHandler, false )
+                }
+                else
+                {
+                    fixedDisplay = 'none'
 
-					elemVis = 'visible'
+                    elemVis = 'visible'
 
-					window.removeEventListener( 'resize', this._resizeHandler )
-				}
+                    window.removeEventListener( 'resize', this._resizeHandler )
+                }
 
-				this.fixed.style.setProperty( 'display', fixedDisplay )
+                this.fixed.style.setProperty( 'display', fixedDisplay )
 
-				this.elem.style.setProperty( 'visibility', elemVis )
+                this.elem.style.setProperty( 'visibility', elemVis )
 
-				this.visible = bool
-			},
+                this.visible = bool
+            },
 
-			scrollHandler: function ()
-			{
-				// vertical scrolling
-				if ( this.elem.getBoundingClientRect().top < parseInt( this.opts.top ) )
-				{
-					if ( !this.visible )
-					{
-						this.setVisible( true )
-					}
-				}
-				else if ( this.visible )
-				{
-					this.setVisible( false )
-				}
-			},
+            scrollHandler: function ()
+            {
+                // vertical scrolling
+                if ( this.elem.getBoundingClientRect().top < parseInt( this.opts.top ) )
+                {
+                    if ( !this.visible )
+                    {
+                        this.setVisible( true )
+                    }
+                }
+                else if ( this.visible )
+                {
+                    this.setVisible( false )
+                }
+            },
 
-			resizeHandler: function ()
-			{
-				this.setPos()
-			}
-		}
+            resizeHandler: function ()
+            {
+                this.setPos()
+            }
+        }
 
-		window.fixel = Fixel
-	}
+        window.fixel = Fixel
+    }
 )( this.window );
